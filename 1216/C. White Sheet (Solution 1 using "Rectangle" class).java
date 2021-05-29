038dc9788e36fe7838af2ed213e03b814c2ce20a
Austin-X <32133198+Austin-X@users.noreply.github.com>
2021-05-29 14:56:40 -0400

    Update C. White Sheet (Solution 1 using "Rectangle" class).java

--- 1216/C. White Sheet (Solution 1 using "Rectangle" class).java
+++ 1216/C. White Sheet (Solution 1 using "Rectangle" class).java
@@ -6,42 +6,23 @@ public class WhiteSheet {
 	static BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 	static StringTokenizer st;
 	static PrintWriter pw = new PrintWriter(System.out);
-	
-	private static final int MV = 1000000;
-	static Rectangle r1;
-	static boolean[] rows = new boolean[MV + 1];
-	
+		
 	public static void main(String[] args) throws IOException {
 		int x1 = readInt(), y1 = readInt(), x2 = readInt(), y2 = readInt();
-		r1 = new Rectangle(x1, y1, x2 - x1, y2 - y1);
+		Rectangle white = new Rectangle(x1, y1, x2 - x1, y2 - y1);
 		x1 = readInt(); y1 = readInt(); x2 = readInt(); y2 = readInt();
-		Rectangle r2 = new Rectangle(x1, y1, x2 - x1, y2 - y1);
+		Rectangle black1 = new Rectangle(x1, y1, x2 - x1, y2 - y1);
 		x1 = readInt(); y1 = readInt(); x2 = readInt(); y2 = readInt();
-		Rectangle r3 = new Rectangle(x1, y1, x2 - x1, y2 - y1);		
-		for (int i = r1.y; i <= r1.y + r1.height; i ++) rows[i] = true;
+		Rectangle black2 = new Rectangle(x1, y1, x2 - x1, y2 - y1);		
 		
-		solve(r2, r3);
-		solve(r3, r2);
-		boolean flag = false;
-		for (int i = r1.y + 1; i <= r1.y + r1.height; i ++) if (rows[i] == true) { flag = true; break; }
-		pw.println(flag ? "YES" :"NO");
+		Rectangle wb1 = white.intersection(black1), wb2 = white.intersection(black2), overlap  = wb1.intersection(wb2);
+		pw.println(getArea(white) > getArea(wb1) + getArea(wb2) - getArea(overlap) ? "YES" : "NO");
 		pw.close();
 	}
 	
-	static void solve(Rectangle r2, Rectangle r3) {
-		Rectangle i1 = r1.intersection(r2), i2 = r1.intersection(r3);
-		if (i1.height > 0 && i1.width > 0) {
-			int x = i1.x, y = i1.y, w = i1.width, h = i1.height;
-			if (x == r1.x) {
-				if (x + w == r1.x + r1.width) {
-					for (int i = y + 1; i <= y + h; i ++) rows[i] = false;
-				} else if (i2.width > 0 && i2.height > 0 && i2.x <= x + w && i2.y <= y && i2.y + i2.height >= y + h && i2.x + i2.width >= r1.x + r1.width) {
-					for (int i = y + 1; i <= y + h; i ++) rows[i] = false;
-				} 
-			} else if (x + w >= r1.x + r1.width && i2.width > 0 && i2.height > 0 && i2.y <= y && i2.y + i2.height >= y + h && i2.x == r1.x && i2.x + i1.width >= x + w) {
-				for (int i = y + 1; i <= y + h; i ++) rows[i] = false;
-			}
-		}
+	static long getArea(Rectangle r) {
+		if (r.height <= 0 || r.width <= 0) return 0;
+		return (long)r.height * r.width;
 	}
 
 	static String next() throws IOException {
