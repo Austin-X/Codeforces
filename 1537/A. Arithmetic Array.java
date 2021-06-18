b5eb1e2016838a7453675bb3f2329334a317abc1
Austin-X <32133198+Austin-X@users.noreply.github.com>
2021-06-18 12:51:06 -0400

    Create A. Arithmetic Array.java

---
+++ 1537/A. Arithmetic Array.java
@@ -0,0 +1,41 @@
+import java.io.*;
+import java.util.*;
+
+public class ArithmeticArray {
+	static BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+	static StringTokenizer st;
+	static PrintWriter pw = new PrintWriter(System.out);
+	
+	public static void main(String[] args) throws IOException {
+		int T = readInt();
+		
+		for (int t = 0; t < T; t ++) {
+			int n = readInt(), sum = 0;
+			for (int i = 0; i < n; i ++) sum += readInt();
+			if (sum < n) pw.println(1);
+			else pw.println(sum - n);
+		}
+		pw.close();
+	}
+
+	static String next() throws IOException {
+		while (st == null || !st.hasMoreTokens()) 
+			st = new StringTokenizer(br.readLine().trim());
+		return st.nextToken();
+	}
+	static long readLong() throws IOException {
+		return Long.parseLong(next());
+	}
+	static int readInt() throws IOException {
+		return Integer.parseInt(next());
+	}
+	static double readDouble() throws IOException {
+		return Double.parseDouble(next());
+	}
+	static char readCharacter() throws IOException {
+		return next().charAt(0);
+	}
+	static String readLine() throws IOException {
+		return br.readLine().trim();
+	}
+}
