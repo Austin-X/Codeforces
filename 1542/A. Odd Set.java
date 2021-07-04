73efff9bd45bbde1f8a3cb65f01705271ddd041f
Austin-X <32133198+Austin-X@users.noreply.github.com>
2021-07-03 21:44:05 -0400

    Create A. Odd Set.java

---
+++ 1542/A. Odd Set.java
@@ -0,0 +1,39 @@
+import java.io.*;
+import java.util.*;
+
+public class OddSet {
+	static BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+	static StringTokenizer st;
+	
+	public static void main(String[] args) throws IOException {
+		int t = readInt();
+		
+		while (t-- > 0) {
+			int n = readInt();
+			int even = 0;
+			for (int i = 0; i < 2 * n; i ++) if (readInt() % 2 == 0) even ++;
+			System.out.println(even == n ? "Yes" : "No");
+		}
+	}
+
+	static String next() throws IOException {
+		while (st == null || !st.hasMoreTokens()) 
+			st = new StringTokenizer(br.readLine().trim());
+		return st.nextToken();
+	}
+	static long readLong() throws IOException {
+		return Long.parseLong(next());
+	}
+	static int readInt() throws IOException {
+		return Integer.parseInt(next());
+	}
+	static double readDouble() throws IOException {
+		return Double.parseDouble(next());
+	}
+	static char readCharacter() throws IOException {
+		return next().charAt(0);
+	}
+	static String readLine() throws IOException {
+		return br.readLine().trim();
+	}
+}
