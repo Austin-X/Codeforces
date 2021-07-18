7b024586134728d062098859ed0466d3a5c1d6bd
Austin-X <32133198+Austin-X@users.noreply.github.com>
2021-07-18 14:08:31 -0400

    Create A. Binary Decimal.java

---
+++ 1530/A. Binary Decimal.java
@@ -0,0 +1,42 @@
+import java.io.*;
+import java.util.*;
+
+public class BinaryDecimal {
+	static BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+	static StringTokenizer st;
+	static PrintWriter out = new PrintWriter(System.out);
+	
+	public static void main(String[] args) throws IOException {
+		int t = readInt();
+		
+		while (t-- > 0) {
+			int n = readInt(), ans = 0;
+			while (n > 0) {
+				ans = Math.max(ans, n % 10);
+				n /= 10;
+			}
+			System.out.println(ans);
+		}
+	}
+
+	static String next() throws IOException {
+		while (st == null || !st.hasMoreTokens()) 
+			st = new StringTokenizer(br.readLine().trim());
+		return st.nextToken();
+	}
+	static long readLong() throws IOException {
+		return Long.parseLong(next());
+	}
+	static int readInt() throws IOException {
+		return Integer.parseInt(next());
+	}
+	static double readDouble() throws IOException {
+		return Double.parseDouble(next());
+	}
+	static char readCharacter() throws IOException {
+		return next().charAt(0);
+	}
+	static String readLine() throws IOException {
+		return br.readLine().trim();
+	}
+}
