3a7c1459505315d742f760939a23693acdb221fb
FLAME <xaustin03@gmail.com>
2023-05-28 10:31:48 -0400

    Create A. Divisible Array.cpp

---
+++ 1828/A. Divisible Array.cpp
@@ -0,0 +1,21 @@
+#include <bits/stdc++.h>
+#define rep(x,y,z) for(ll x=y;x<=z;x++)
+using namespace std;
+typedef long long ll;
+
+void solve() {
+    int n;
+    cin >> n;
+    rep(i, 1, n) cout << i * 2 << ' ';
+    cout << '\n';
+}
+
+int main() {
+    cin.sync_with_stdio(0);
+    cin.tie(0);
+    
+    int t;
+    cin >> t;
+    while (t-- > 0) solve();
+    return 0;
+}
