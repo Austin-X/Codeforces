c8686574b5f1b39b3f9bc0bc4c8789b1d0738947
FLAME <xaustin03@gmail.com>
2023-07-29 14:35:38 -0400

    Create B. Longest Divisors Interval.cpp

---
+++ 1855/B. Longest Divisors Interval.cpp
@@ -0,0 +1,22 @@
+#include <bits/stdc++.h>
+#define rep(x,y,z) for(ll x=y;x<=z;x++)
+#define pii pair<int, int>
+using namespace std;
+typedef long long ll;
+
+void solve() {
+    ll n, ans = 1;
+    cin >> n;
+    while (n % ans == 0) ans ++;
+    cout << (ans - 1) << '\n';
+}
+
+int main() {
+    cin.sync_with_stdio(0);
+    cin.tie(0);
+
+    int t;
+    cin >> t;
+    while (t-- > 0) solve();
+    return 0;
+}
